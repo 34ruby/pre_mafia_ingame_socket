bdc00881bf00d4d8694cb210b77872ae4936e102
34ruby <ixia5027@gmail.com>
2022-03-23 19:21:01 +0900

    connection to Nuxt.js

--- client/components/Tutorial.vue
+++ client/components/Tutorial.vue
@@ -1,14 +1,18 @@
 <!-- Please remove this file from your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-  <div>
-    <canvas ref="game" width="640" height="480" style="border: 1px solid black;">
+  <!-- <input type="string" /> -->
+  <div class="box-content h-128 w-128 p-4 border-4">
+    <!-- <canvas ref="game" width="640" height="480" style="border: 1px solid black;">
     </canvas>
-
       <button v-on:click="move('right')">right</button>
-      <div v-on:click="move('right')">{{this.position.z}}</div>
-
+      <div v-on:click="move('right')">{{this.position.z}}</div> -->
+      <!-- <label class="block"> -->
+      <input type="text" :value="message" @input="message=$event.target.value">
+      <div class="text-8xl">{{this.flowMessage}}</div>
+      <div class="text-2xl">당신은 : {{this.playerJob[this.message-1]}}</div>
   </div>
+      <!-- </label> -->
   </div>
 </template>
 
@@ -19,29 +23,30 @@ export default {
   data() {
     return {
       socket: {},
-      context: {},
-      position: {
-        x: 0,
-        y: 0,
-        z: '',
-      }
+      playerJob: [],
+      message: 0,
+      flowMessage: ''
     }
   },
   created() {
     this.socket = io("http://localhost:3300")
   },
   mounted() {
-    this.context = this.$refs.game.getContext("2d")
-    this.socket.on("position", data => {
-      this.position = data
-      this.context.clearRect(0,0,this.$refs.game.width, this.$refs.game.height)
-      this.context.fillRect(this.position.x, this.position.y, 20, 20)
+    // this.context = this.$refs.game.getContext("2d")
+    // this.socket.on("position", data => {
+    //   this.position = data
+    //   this.context.clearRect(0,0,this.$refs.game.width, this.$refs.game.height)
+    //   this.context.fillRect(this.position.x, this.position.y, 20, 20)
+    // })
+    this.socket.on("playerJob", data => {
+      this.playerJob = data
     })
+
   },
   methods: {
-    move(direction) {
-      this.socket.emit("move", direction);
-    }
+    // move(direction) {
+    //   this.socket.emit("move", direction);
+    // }
   }
 }
 </script>
--- server/app.js
+++ server/app.js
@@ -2,22 +2,51 @@ const Express = require("express")();
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http, { cors: { origin: "*" } });
 
-var position = {
-    x: 200,
-    y: 200,
-    z: 'hello'
+// let flowMessage = '직업 배분중 잠시만 기다려주세요'
+let playerJob = []
+
+console.log('직업배분 시작')
+const candidates = Array(10).fill().map((v, i) => v = i)
+while(candidates.length > 0) {
+    let num = Math.floor(Math.random() * candidates.length);
+    let leftNum = candidates.splice(num, 1)[0]
+    playerJob.push(leftNum)  
+}
+console.log(playerJob)
+for (let i = 0; i < 10; i++) {
+    if(playerJob[i] == 0 | playerJob[i] == 1) {
+        playerJob[i] = '마피아'
+    } else if (playerJob[i] == 2) {
+        playerJob[i] = '경찰'
+    } else if (playerJob[i] == 3) {
+        playerJob[i] = '의사'
+    } else {
+        playerJob[i] = '시민'
+    }
 }
 
+console.log(playerJob)
+// let position = {
+//     x: 200,
+//     y: 200,
+//     z: 'hello',
+// }
+// console.log(position)
+
 Socketio.on("connection", socket => {
-    socket.emit("position", position)
-    socket.on("move", data => {
-        position.x += 5;
-        position.z = '안녕!'
-        Socketio.emit("position", position);
-        console.log("move right")
-    })
+    socket.emit("playerJob", playerJob)
 })
 
+
+// Socketio.on("connection", socket => {
+//     socket.emit("position", position)
+//     socket.on("move", data => {
+//         position.x += 5;
+//         position.z = '안녕!'
+//         Socketio.emit("position", position);
+//         console.log("move right")
+//     })
+// })
 
 Http.listen(3300, () => {
     console.log("Listening at 3300...")
